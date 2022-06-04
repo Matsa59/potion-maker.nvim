f03b38cac3a93f35cbe5f41a68e873b37c1dba91
matsa <alexandre.lprtr@gmail.com>
2022-06-04 09:31:27 -0400

    Add confirmation system

--- lua/potion-maker/init.lua
+++ lua/potion-maker/init.lua
@@ -8,6 +8,13 @@ local is_umbrella = function(file_path)
   return string.find(file_path, '^apps/') ~= nil
 end
 
+local function file_exists(file_path)
+  local f=io.open(file_path,"r")
+  if f~=nil then io.close(f) return true else return false end
+end
+
+--- Returns if the file_path is a test or src file.
+--@param file_path file path to test.
 M.is_test_file = function(file_path)
   local regex_expr
 
@@ -20,6 +27,8 @@ M.is_test_file = function(file_path)
   return string.find(file_path, regex_expr) ~= nil
 end
 
+--- Returns the associated src/test file to the specified {file_path} file.
+-- @param file_path file path of the file we want to switch from.
 M.get_toggle_test_file = function(file_path)
   local base_expression, replace
 
@@ -52,6 +61,8 @@ M.get_toggle_test_file = function(file_path)
   return toggled_file
 end
 
+--- Switch between src and test file.
+--If the file doesn't exist, it prompts a confirmation.
 M.toggle_test_file = function()
   local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
 
@@ -60,11 +71,20 @@ M.toggle_test_file = function()
   end
 
   local current_file = get_current_file_path()
-
   local toggled_file = M.get_toggle_test_file(current_file)
 
-  vim.cmd(':e ' .. toggled_file, false)
-  vim.cmd('silent exec "!mkdir -p %:h"', false)
+  if file_exists(toggled_file) then
+    vim.cmd(':e ' .. toggled_file, false)
+  else
+    local msg = "File doesn't exist. Do you want to create it?"
+    local response = vim.fn.confirm(msg, '&y\n&n', 1)
+
+    if response == 1 then
+      vim.cmd(':e ' .. toggled_file, false)
+      vim.cmd('silent exec "!mkdir -p %:h"', false)
+      vim.cmd('silent :w')
+    end
+  end
 end
 
 return M
